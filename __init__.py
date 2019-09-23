36c65ae0f011c92d01ac61ee8677f54fe230f8c5
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-23 01:26:28 +0100

    Finally finish with __init__.py and the tweetBot module

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-print(f'Invoking __init__.py for {__name__}')
-
 import tweetBot.main
+
+# Add aliases when this module is imported
 tweetBot.ebooksTweet = tweetBot.main.ebooksTweet
 tweetBot.genreTweet = tweetBot.main.genreTweet
 tweetBot.postTweet = tweetBot.main.postTweet
