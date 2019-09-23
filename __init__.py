ff6eabc72e2bb7c7db4a4cead130f85833932a90
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-23 01:24:53 +0100

    Attempt to clean up the modules for easier functions

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 print(f'Invoking __init__.py for {__name__}')
 
-import tweetBot.main 
+import tweetBot.main
+tweetBot.ebooksTweet = tweetBot.main.ebooksTweet
+tweetBot.genreTweet = tweetBot.main.genreTweet
+tweetBot.postTweet = tweetBot.main.postTweet
