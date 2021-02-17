2ba3b119809b011b7481be5a942e363808d49907
Jademalo <386846+Jademalo@users.noreply.github.com>
2021-02-17 15:19:32 +0000

    Fix for undefined function

--- __init__.py
+++ __init__.py
@@ -3,5 +3,6 @@ import tweetBot.generator, tweetBot.poster
 # Add aliases when this module is imported
 tweetBot.ebooksGen = tweetBot.generator.ebooksGen
 tweetBot.genreGen = tweetBot.generator.genreGen
+tweetBot.variantGen = tweetBot.generator.variantGen
 
 tweetBot.postTweet = tweetBot.poster.postTweet
