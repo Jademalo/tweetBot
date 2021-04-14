b2d296e4456b86d164761d2853140571a887ffd4
Jademalo <386846+Jademalo@users.noreply.github.com>
2021-04-14 16:07:25 +0100

    Redesign poster so keys are provided, to be more modular

--- poster.py
+++ poster.py
@@ -1,7 +1,6 @@
 #-------------------------------------------------------------------------------
 # Imports
 #-------------------------------------------------------------------------------
-from . import keys
 import tweepy
 import random
 
@@ -10,9 +9,8 @@ import random
 # Twitter related functions
 #-------------------------------------------------------------------------------
 
-# Make a tweepy instance using the keys from the provided account variable
-def setTwitter(account):
-    consumer_key, consumer_secret, access_token, access_token_secret = keys.returnKeys(account)
+# Make a tweepy instance using the keys from the provided account variables
+def setTwitter(consumer_key, consumer_secret, access_token, access_token_secret):
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -21,8 +19,8 @@ def setTwitter(account):
     return api
 
 # Post a tweet using the text, the account, and optional frequency
-def postTweet(tweetText, account, postFreq=1):                                  # postFreq is optional, if not passed it will always be 1 and always post
-    twitter = setTwitter(account)
+def postTweet(tweetText, consumer_key, consumer_secret, access_token, access_token_secret, postFreq=1):                                  # postFreq is optional, if not passed it will always be 1 and always post
+    twitter = setTwitter(consumer_key, consumer_secret, access_token, access_token_secret)
 
     # If postFreq is set to 0, don't do anything.
     if postFreq == 0:
